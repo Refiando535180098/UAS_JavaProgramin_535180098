76520e8be5a80f389b677d6e696d499c9c629ae0
Refiando535180098 <refiando.535180098@stu.untar.ac.id>
2022-06-02 23:33:24 +0700

    Tugas_UAS_Java_Programing_535180098
    Refiando Vandana - 535180098
    
    Tugas UAS Java Programing aplikasi kasir berbasis web.

---
+++ src/main/java/com/example/AplikasiKasirFinal/AplikasiKasirFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.AplikasiKasirFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiKasirFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiKasirFinalApplication.class, args);
+	}
+
+}
